dd90c1f0056b0d82cad81d2e591b7ca8f80e5d1c
Jominsu <whalstn9@hecto.co.kr>
2024-08-21 17:00:19 +0900

    [Axios] 인터셉터로 응답, 요청 로그 추가.

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,49 @@
 import axios from "axios";
 
+const baseURL = "http://192.168.13.54:3000";
 export const AxiosInstance = axios.create({
-  baseURL: "http://192.168.13.54:3000",
+  baseURL,
   headers: { "Content-Type": "application/json" },
   timeout: 10 * 1000,
 });
+
+AxiosInstance.interceptors.request.use(
+  (config) => {
+    console.log(
+      "Request:",
+      config.method?.toUpperCase(),
+      baseURL + config.url,
+      "Request Data:",
+      config.data
+    );
+    return config;
+  },
+  (error) => {
+    console.error("Request Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    console.log(
+      "Response:",
+      response.status,
+      baseURL + response.config.url,
+      "Response Data:",
+      response.data
+    );
+    return response;
+  },
+  (error) => {
+    console.error(
+      "Response Error:",
+      error.response?.status,
+      error.response?.config.url,
+      "Error Data:",
+      error.response?.data
+    );
+    return Promise.reject(error);
+  }
+);
