22d21fafece603a5dae5f39bbf468b0b6fa05c99
Jominsu <whalstn9@hecto.co.kr>
2024-08-27 09:56:24 +0900

    [API]refetchOnWindowFocus 조건 정의  - 포커스가 잡힐 떄마다 데이터 조회 요청이 들어가서 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,31 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import dayjs from "dayjs";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: (query) => {
+        // 현재 시간과 마지막 데이터 업데이트 시간의 차이를 계산
+        const lastUpdated = dayjs(query.state.dataUpdatedAt);
+        const now = dayjs();
+
+        const minutesSinceLastUpdate = now.diff(lastUpdated, "minute");
+        if (minutesSinceLastUpdate >= 1) {
+          // 1분 이상 지났을 때 윈도우 focus되면 리페치
+          console.log("-----------REFETCHING DATA WITH FOUCS-----------");
+          return true;
+        } else {
+          return false;
+        }
+      },
+    },
+  },
+});
 
 root.render(
   <QueryClientProvider client={queryClient}>
